d2d7bd18ebce9c8f20b924e8a3f5671006206674
muqsith03 <alimuqsith03@gmail.com>
2020-02-16 21:50:21 +0700

    tambah materi php

--- belajar_php.php
+++ belajar_php.php
@@ -5,22 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>belajar PHP</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
-    <h3>Daftar nama peserta</h3>
-    <table border="1">
-        <tr>
-            <th>No</th>
-            <th>nama peserta</th>
-        </tr>
-        <tr>
-            <?php 
-            for($i = 0; $i < 10; $i++){
-                echo "<tr><td>$i</td><td>nama peserta - $i</td></tr>";
-            }
-            
-            ?>
-        </tr>
-    </table>
+    <div class="container">
+        <div class="row pt-3">
+            <div class="col-md-6">
+                <h4>tipe-tipe data pada PHP</h4>
+                <p>dalam php terdapat 8 jenis data yang bisa dikelompokan menjadi 3 bagian</p>
+                <p>tipe data dasar / data primitive / tipe data scalar
+                    <ul>
+                        <li>integer</li>
+                        <li>float</li>
+                        <li>boolean</li>
+                        <li>string</li>
+                    </ul>
+                </p>
+                <p>tipe data composite
+                    <ul>
+                        <li>array</li>
+                        <li>object</li>
+                    </ul>
+                </p>
+                <p>tipe data khusus
+                    <ul>
+                        <li>resource</li>
+                        <li>null</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+
+
+
+
+
+
+
+    </div>
+    
 </body>
 </html>
