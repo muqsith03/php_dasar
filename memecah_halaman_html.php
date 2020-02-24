0450182e2587bcfebb56c46efe8db6912ac52c3d
muqsith03 <alimuqsith03@gmail.com>
2020-02-25 06:26:56 +0700

    tambah materi fungsi date

--- memecah_halaman_html.php
+++ memecah_halaman_html.php
@@ -23,6 +23,37 @@
         //echo "teks";
         //header("Location: http://www.google.com");
     ?>
+
+    <h4>fungsi date dan time</h4>
+    <p>untuk memproses tanggal kita menggunakan fungsi date dan time. di dalam php manual, date and time function bisa diakses dari function reference-> date and time related extentions-> date and time-> date/time function. fungsi time() akan mengembalikan nilai angka integer yang disebut dengan unix timestamp. unix timestamp adalah angka dalam satuan detik sejak tanggal 1 januari 1970, pukul 00:00:00 GMT. dengan sistem perhitungn ini, kita bisa mencari waktu untuk minggu depan, kmarein, besok atau lusa. caranya dengan tambah atau kurangkan nilai unix timestamp seperti contoh : </p>
+    <?php 
+        $minggu_depan = time() + (7 * 24 * 60 * 60);
+        echo $minggu_depan;
+        echo "<br>";
+        $kemarin = time() - (1*24*60*60);
+        echo $kemarin;
+    ?>
+    <h5>fungsi date</h5>
+    <p>format penulisan date(string$format[,int $timestamp = time()]) : string</p>
+    <p>hasil dari time diatas belum berbentuk sebuah "tanggal". untuk menampilkan dan memformat tanggal php menyediakan fungsi date()</p>
+    <p>fungsi date butuh 1 argumen utama dan 1 opsional, argumen peertama diisi dengan format tampilan tanggal, dan argumen kedua diisi dengan anga unix timestamp. apabila tidak ditulis secara default akan diapaki tanggal sekarang. berikut contohnya</p>
+    <p><?php 
+        echo date("d-m-Y", $minggu_depan);
+        echo "<br>";
+        echo date("d-m-Y");
+        echo "<h4>membuat tanggal format indonesia</h4> <br>";
+        $nama_hari = ["ahad", "senin", "selasa", "rabu", "kamis", "jum'at", "sabtu"];
+        $nama_bulan = ["Januari", "Februari", "Maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "desember"];
+
+        $hari    = date("w");
+        $tanggal = date("j");
+        $bulan   = date("n");
+        $tahun = date("Y");
+
+        echo "tanggal hari ini adalah : $nama_hari[$hari]-$tanggal-$nama_bulan[$bulan]-$tahun";
+        
+    ?></p>
+    
 </article>
 
 <?php 
