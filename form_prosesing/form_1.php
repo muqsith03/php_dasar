a53689a99c082bdaf16e3ecf089b5da3585cc106
muqsith03 <alimuqsith03@gmail.com>
2020-03-01 17:19:32 +0700

    tambah materi prosesing user friendly

--- form_prosesing/form_1.php
+++ form_prosesing/form_1.php
@@ -1,3 +1,25 @@
+<?php 
+    if(isset($_GET["pesan"])){
+        $pesan = "<p>{$_GET["pesan"]}</p>";
+    } else{
+        $pesan = "";
+    }
+
+    // ambil nilai nama jika ada
+    if(isset($_GET["nama"])){
+        $nilai_nama = $_GET["nama"];
+    }else{
+        $nilai_nama = "";
+    }
+
+    // ambil nilai email jika ada
+    if(isset($_GET["email"])){
+        $nilai_email = $_GET["email"];
+    } else{
+        $nilai_email = "";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +33,13 @@
 <body>
     <div class="container">
         <h4>Latihan Form : menampilkan pesan kesalahan form</h4>
-        
+        <form action="proses_1.php" method="POST">
+            <?=$pesan; ?>
+            <p>Nama : <input type="text" name="nama" id="nama"<?=$nilai_nama ?>></p>
+            <p>email : <input type="text" name="email" id="email" <?=$nilai_email ?>></p>
+            <p><input type="submit" name="submit" id="submit" value="Submit"></p>
+        </form>
+
     </div>
 </body>
 </html>
---
+++ form_prosesing/proses_1.php
@@ -0,0 +1,50 @@
+<?php 
+
+    if(!isset($_POST["submti"])){
+        header("Location: form_1.php");
+
+    }
+    // ambil nilai dari POST
+    $nama = trim($_POST["nama"]);
+    $email = trim($_POST["email"]);
+
+    // siapkan nilai untuk dikirim kembali
+    $query_nama = "nama=".urlencode($nama);
+    $query_email = "email=".urlencode($email);
+    $isi_form = "&$query_nama&$query_email";
+
+    // cek apakah form nama kosong atau tidak
+    if(empty($email) & empty($nama)){
+        $pesan= urlencode("nama dan email masih kosong");
+        header("Location: form_1.php?pesan={$pesan}{$isi_form}");
+        die();
+    }
+    if(empty($nama)){
+        $pesan = urlencode("form nama harus diisi");
+        header("Location: form_1.php?pesan={$pesan}{$isi_form}");
+        die();
+    }
+    // cek apakah form email kosong atau tidak
+    if(empty($email)){
+        $pesan = urlencode("form email harus diisi");
+        header("Location: form_1.php?pesan=$pesan");
+        die();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proses form_1</title>
+</head>
+<body>
+    <?php 
+        echo "nama : $nama <br>";
+        echo "email : $email <br>";
+        if(isset($_POST["belajar"])){
+            echo "belajar ".$_POST["belajar"]."<br>";
+        }
+    ?>
+</body>
+</html>
