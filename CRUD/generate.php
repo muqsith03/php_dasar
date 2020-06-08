80d200f22078bba48c434170ff93c3f5e283966b
muqsith03 <alimuqsith03@gmail.com>
2020-06-08 12:25:43 +0700

    tambah materi kampusku

---
+++ CRUD/generate.php
@@ -0,0 +1,65 @@
+<?php 
+    // buat koneksi dengan database
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $link = mysqli_connect($dbhost,$dbuser,$dbpass);
+
+    // cek koneksi, tampilkan kesalahan jika gagal
+    if(!$link){
+        die("koneksi dengan database gagal: ".mysqli_connect_errno($link)." - ".mysqli_connect_error($link));
+    }
+
+    // buat database kampusku jika belum ada
+    $query = "CREATE DATABASE IF NOT EXISTS kampusku";
+    $result = mysqli_query($link, $query);
+    // cek apakah query berhasil
+    if(!$result){
+        die("Query Error: ".mysqli_errno($link)." - ".mysqli_error($link));
+    } else {
+        echo "database <b>'kampusku'</b> berhasil dibuat...<br>";
+    }
+    // pilih database kampusku
+    $result = mysqli_select_db($link,"kampusku");
+    if(!$result){
+        die("Quey Error: ".mysqli_errno($link)." - ".mysqli_error($link));
+    } else {
+        echo "Database 'kampusku' berhasil dipilih... <br>";
+    }
+
+    // cek apakah tabel mahasiswa sudah ada, jika ada hapus table 
+    $query = "DROP TABLE IF EXISTS mahasiswa";
+    $hasil_query = mysqli_query($link, $query);
+
+    if(!$hasil_query){
+        die("query error: ".mysqli_errno($link)." - ".mysqli_error($link));
+    } else {
+        echo "tabel <b>'mahasiswa'</b> berhasil dihapus...";
+    }
+
+    // buat query untuk create table mahasiswa
+    $query = "CREATE TABLE mahasiswa (nim char(8), nama varchar(60), tempat_lahir varchar(70), tanggal_lahir DATE, fakultas varchar(60), jurusan varchar(70), ipk decimal(3,2) PRIMARY KEY (nim))";
+    $hasil_query = mysqli_query($link,$query);
+
+    if(!$hasil_query){
+        die("Query error: ".mysqli_errno($link). " - ".mysqli_error($link));
+    } else {
+        echo "tabel <b>'mahasiswa'</b> berhasil dibuat...<br>";
+    }
+
+    //buat query untuk insert data ke tabel mahasiswa
+    $query = "INSERT INTO mahasiswa VALUES('1400501','riana putria','padang','1996-11-23','FMIPA','kimia',3.1),('150210044','rudi permana','bandung','1994-08-22','FASILKOM','ilmu komputere',2.9), ('15003036','sari citra lestari','jakarta','1997-12-31','ekonomi','manajemen',3.5), ('15002032','rina kumala sari','jakarta','1997-06-28','ekonomi','akuntansi',3.4), ('13012012','james situmoang','medan','1995-04-02','kedokteran','kedokteran gigi',2.7)";
+
+    $hasil_query =mysqli_query($link,$query);
+
+    if(!$hasil_query){
+        die("query error :"/mysqli_errno($link)." - ".mysqli_error($link));
+    } else {
+        echo "table <b>'mahasiswa' berhasil diisii...</b>";
+    }
+
+    // membuat tabel admin
+    // cek apakah tabel admin sudah ada ? jika ada hapus tabel
+    $query = "DROP TABLE IF EXISTS admin";
+    $hasil_query = mysqli_query($link,$query);
+?>
--- mysql_dasar/input_data_prepare.php
+++ mysql_dasar/input_data_prepare.php
@@ -1,30 +1,35 @@
-<?php 
-// buat koneksi kedatabase
-$koneksi = mysqli_connect("localhost","root","","universitas");
-// cek apakah berhasil terhubung ke database
-if(!$koneksi){
-    die("koneksi gagal:".mysqli_connect_errno($koneksi)."- ".mysqli_connect_error($koneksi));
-}
+<?php
+    // buaut koneksi ke database
+    $koneksi = mysqli_connect("localhost","root","","universitas");
+    // cek apakah berasil terhubung ke database
+    if(!$koneksi){
+        die("gagal terhubuung database: ".mysqli_connect_errno($koneksi)." - ".mysqli_error($koneksi));
+    }
 
-// siapkan data
-$nim = "143138";
-$nama = "fajar";
-$tempat_lahir = "tangerang";
-$tanggal_lahir = "1995-04-15";
-$fakultas = "teknik informatika";
-$jurusan = "system architecture";
-$ipk = 3.80;
+    // buat data untuk diinput
+    $nim = "143138";
+    $nama = "fajar";
+    $tempat_lahir = "tangerang";
+    $tanggal_laihr = "1995-04-15";
+    $fakultas = "teknik informatika";
+    $jurusan = "system arcitecture";
+    $ipk = "3.50";
 
-// buat query prepare
-$query = "INSERT INTO mahasiswa VALUES (?,?,?,?,?,?,?)";
-$stmt = mysqli_prepare($koneksi,$query);
-
-// hubungkan data dengan prepared statmend :bind
-mysqli_stmt_bind_param($stmt,"ssssssi",$nim,$nama,$tempat_lahir,$tanggal_lahir,$fakultas,$jurusan,$ipk);
-
-// jalankan query execute
-$sukses = mysqli_stmt_execute($stmt);
-// cek apakah berhaisl errornya
-
-
+    // buat query prepare
+    $quey = "INSERT INTO mahasiswa VALUE(?,?,?,?,?,?,?)";
+    $stmt = mysqli_prepare($koneksi,$quey);
+    // hubungkan data dengan prepare statment :bind
+    mysqli_stmt_bind_param($stmt, "ssssssi", $nim,$nama,$tempat_lahir,$tanggal_laihr,$fakultas,$jurusan,$ipk);
+    // jalankan quey execute
+    $sukses = mysqli_stmt_execute($stmt);
+    // cek apakah query berasil
+    if(!$sukses){
+        die("query gagal: ".mysqli_errno($koneksi)." - ".mysqli_error($koneksi));
+    } else{
+        echo "penambaan ".mysqli_stmt_affected_rows($stmt)." data berasil<br>";
+    }
+    // tutup statment
+    mysqli_stmt_close($stmt);
+    // tutup koneksi ke database
+    mysqli_close($koneksi);
 ?>
